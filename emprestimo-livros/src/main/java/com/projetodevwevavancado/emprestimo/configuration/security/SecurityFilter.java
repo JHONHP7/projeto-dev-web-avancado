fc284772a6b89e1e67172f6e1d69d155d55b7065
Jhonatan linux <jhonatansilva@id.uff.br>
2024-11-18 23:50:19 -0300

    fix: ajuste no SecurityFilter

--- emprestimo-livros/src/main/java/com/projetodevwevavancado/emprestimo/configuration/security/SecurityFilter.java
+++ emprestimo-livros/src/main/java/com/projetodevwevavancado/emprestimo/configuration/security/SecurityFilter.java
@@ -17,35 +17,28 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 @Component
-public class SecurityFilter extends OncePerRequestFilter{
-	
-	@Autowired
-	TokenService service;
-	
-	@Autowired
-	UserRepository repository;
+public class SecurityFilter extends OncePerRequestFilter {
+    @Autowired
+    TokenService tokenService;
+    @Autowired
+    UserRepository userRepository;
 
-	@Override
-	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
-			throws ServletException, IOException {
-		
-		var token = this.recoverToken(request);
-		if(token != null ) {
-			var email = service.validateToken(token);
-			UserDetails user = repository.findByEmail(email);
-			
-			var authentication = new UsernamePasswordAuthenticationToken(null, user.getAuthorities());
-			SecurityContextHolder.getContext().setAuthentication(authentication);
-		}
-		filterChain.doFilter(request, response);
-		
-	}
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        var token = this.recoverToken(request);
+        if(token != null){
+            var email = tokenService.validateToken(token);
+            UserDetails user = userRepository.findByEmail(email);
 
-	private String recoverToken(HttpServletRequest request) {
-		var authHeader = request.getHeader("Authorization");
-		if(authHeader == null) {
-			return null;
-		}
-		return authHeader.replace("Bearer ", "");
-	}
+            var authentication = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+        filterChain.doFilter(request, response);
+    }
+
+    private String recoverToken(HttpServletRequest request){
+        var authHeader = request.getHeader("Authorization");
+        if(authHeader == null) return null;
+        return authHeader.replace("Bearer ", "");
+    }
 }
