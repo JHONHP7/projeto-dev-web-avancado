2df42682fa395166c4a1bd55b28ba4745fb0ce88
jhonatan-note-ubuntu <jhonatan.oliveira.dev@gmail.com>
2024-11-20 21:09:24 -0300

    chore: add classes para lidar com erros de banco de dados

--- emprestimo-livros/src/main/java/com/projetodevwevavancado/emprestimo/api/resource/handler/ResourceExceptionHandler.java
+++ emprestimo-livros/src/main/java/com/projetodevwevavancado/emprestimo/api/resource/handler/ResourceExceptionHandler.java
@@ -1,11 +1,13 @@
 package com.projetodevwevavancado.emprestimo.api.resource.handler;
 
+import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import com.projetodevwevavancado.emprestimo.api.resource.handler.exceptions.AuthenticationFailedException;
+import com.projetodevwevavancado.emprestimo.api.resource.handler.exceptions.DataNotFoundException;
 import com.projetodevwevavancado.emprestimo.api.resource.handler.exceptions.EmailAlreadyExistsException;
 import com.projetodevwevavancado.emprestimo.commons.util.ApiResponse;
 
@@ -29,5 +31,18 @@ public class ResourceExceptionHandler {
 	    ApiResponse response = new ApiResponse("Ocorreu um erro interno. Tente novamente mais tarde.", false);
 	    return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
 	}
+	
+	@ExceptionHandler(DataAccessException.class)
+	public ResponseEntity<ApiResponse> handleDataAccessException(DataAccessException ex) {
+	    ApiResponse response = new ApiResponse("Erro ao acessar o banco de dados. Tente novamente mais tarde.", false);
+	    return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
+	}
+
+	@ExceptionHandler(DataNotFoundException.class)
+	public ResponseEntity<ApiResponse> handleDataNotFoundException(DataNotFoundException ex) {
+	    ApiResponse response = new ApiResponse(ex.getMessage(), false);
+	    return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
+	}
+
 
 }
