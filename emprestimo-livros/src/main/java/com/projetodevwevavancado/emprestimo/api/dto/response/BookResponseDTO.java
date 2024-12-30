7e937efee03aa7bc9668c91318d8411a8ed33ff4
jhonatan-note-ubuntu <jhonatan.oliveira.dev@gmail.com>
2024-12-29 23:30:07 -0300

    feat(BookResponseDTO): enhance BookResponseDTO to include additional fields and constructor for BookEntity conversion
    - Added fields: id, quantidadeExemplares, and genero.
    - Implemented a constructor to initialize BookResponseDTO from a BookEntity instance.
    - Updated date formatting for dataPublicacao to "dd/MM/yyyy" for consistency.

--- emprestimo-livros/src/main/java/com/projetodevwevavancado/emprestimo/api/dto/response/BookResponseDTO.java
+++ emprestimo-livros/src/main/java/com/projetodevwevavancado/emprestimo/api/dto/response/BookResponseDTO.java
@@ -2,6 +2,11 @@ package com.projetodevwevavancado.emprestimo.api.dto.response;
 
 import java.util.Date;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.projetodevwevavancado.emprestimo.entity.BookEntity;
+
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -9,10 +14,32 @@ import lombok.Setter;
 @Setter
 public class BookResponseDTO {
 
+    private Long id;
     private String titulo;
     private String autor;
     private String isbn;
     private Boolean disponivel;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy", timezone = "GMT-3")
+    @Temporal(TemporalType.DATE)
     private Date dataPublicacao;
+    private int quantidadeExemplares;
+    private String genero;
 
+    /**
+     * Constructor for the BookResponseDTO class that converts a BookEntity object into a BookResponseDTO.
+     * This initializes the fields of BookResponseDTO using the corresponding attributes of the BookEntity instance.
+     * Construtor que aceita a entidade para conversão
+     *
+     * @param bookEntity the BookEntity instance containing the data to populate the BookResponseDTO fields
+     */
+    public BookResponseDTO(BookEntity bookEntity) {
+        this.id = bookEntity.getId();
+        this.titulo = bookEntity.getTitulo();
+        this.autor = bookEntity.getAutor();
+        this.isbn = bookEntity.getIsbn();
+        this.disponivel = bookEntity.getDisponivel();
+        this.quantidadeExemplares = bookEntity.getQuantidadeExemplares();
+        this.dataPublicacao = bookEntity.getDataPublicacao();
+        this.genero = bookEntity.getGeneroNome();
+    }
 }
