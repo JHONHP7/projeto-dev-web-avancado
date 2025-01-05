5395aae2a53959b9787c0bb353c5ff59f0fe283f
Jhonatan linux <jhonatansilva@id.uff.br>
2025-01-05 15:04:49 -0300

    fix(LoanService): reset user's suspendedUntil date when processing loan requests

--- emprestimo-livros/src/main/java/com/projetodevwevavancado/emprestimo/service/LoanService.java
+++ emprestimo-livros/src/main/java/com/projetodevwevavancado/emprestimo/service/LoanService.java
@@ -148,6 +148,7 @@ public class LoanService {
 				.orElseThrow(() -> new ResourceNotFoundException("Usuário não encontrado"));
 
 		checkUserSuspension(user);
+		user.setSuspendedUntil(null);
 		checkActiveLoans(loanRequestDTO.idUser());
 		checkDuplicateLoan(loanRequestDTO.idUser(), loanRequestDTO.idBook());
 
